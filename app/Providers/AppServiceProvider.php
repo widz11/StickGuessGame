1faa0c634a7175d7e1a58661a52e4af3f8302eae
Widya Mas S <widyamasseptiawan@gmail.com>
2019-04-03 22:31:49 +0700

    set app https bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      \URL::forceRootUrl(\Config::get('app.url'));
-      if (str_contains(\Config::get('app.url'), 'https://')) {
-          \URL::forceScheme('https');
-      }
+        //
     }
 }
